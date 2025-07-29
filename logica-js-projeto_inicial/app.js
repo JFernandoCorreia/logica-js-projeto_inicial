f1595d2662ec7bd1dbaeac89e2cf299ff72a57b1
JFernando <almo_fernando@hotmail.com>
2025-07-29 20:58:33 -0300

    new commit

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,8 @@
+alert('Boas vindas ao Jogo do número secreto!');
+let numeroSecreto =  29;
+let chute = prompt('Escolha um número entre 1 e 30');
+
+if (chute == numeroSecreto) {
+    console.log('Parabéns, você descobriu o número secreto! (29)');
+}
+
