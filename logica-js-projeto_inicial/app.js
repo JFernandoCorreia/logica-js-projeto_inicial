56e30a93a72e8c25b442df63b490861c06c57339
JFernando <almo_fernando@hotmail.com>
2025-07-30 21:42:12 -0300

    final commit

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,8 +1,38 @@
 alert('Boas vindas ao Jogo do número secreto!');
-let numeroSecreto =  29;
-let chute = prompt('Escolha um número entre 1 e 30');
+let numeroMaximo = 50;
+let numeroSecreto =  Math.floor(Math.random() * numeroMaximo) + 1; // Gera um número aleatório entre 1 e 30
+console.log('Número secreto:', numeroSecreto);
 
-if (chute == numeroSecreto) {
-    console.log('Parabéns, você descobriu o número secreto! (29)');
+let chute;
+console.log('Valor do chute:', chute);
+
+let tentativas = 1;
+
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+
+    if (chute == numeroSecreto) {
+        break;
+    } else { 
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        tentativas++;
+        console.log(`Tentativa ${tentativas}: chute foi ${chute}`);
+    }
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Parabéns! Você acertou o número secreto ${numeroSecreto} em ${tentativas} ${palavraTentativa}!`);
+
+//if (tentativas > 1) {
+//    alert(`Parabéns! Você acertou o número secreto ${numeroSecreto} em ${tentativas} tentativas!`);
+//}   else {
+//    alert(`Parabéns! Você acertou o número secreto ${numeroSecreto} na primeira tentativa!`);
+//}
+console.log('Fim do jogo');
+alert('Fim do jogo');
+alert('Obrigado por jogar!');
 
